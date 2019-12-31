b0e5582911458e373562451f3fed366e65669cb4
Alan Vieceli <alansvieceli@gmail.com>
2019-12-30 23:33:29 -0300

    Busca no grid

--- SCA.Web/wwwroot/js/site.js
+++ SCA.Web/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#buscar").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#dados tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
